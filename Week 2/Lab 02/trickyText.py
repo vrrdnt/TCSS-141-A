2a889d6c1da691da5c951c2758019d77adccb9fc
Will Swoveland <will.arctic@gmail.com>
2020-10-26 20:11:57 -0700

    Create trickyText.py

---
+++ Week 2/Lab 02/trickyText.py
@@ -0,0 +1,13 @@
+# Will Swoveland
+
+# Clarification - the \ character at the end of each line allows me to
+# keep the string going on the next line. It tells the python interpreter
+# to keep building the string, even though it's been stopped. This
+# makes it so that I only need to use one print statement.
+
+print("\'Would you tell me, please, which way I ought to go from here?\'\n\
+\'That depends a good deal on where you want to get to,\' said the Cat.\n\
+\'I don\'t much care where--\' said Alice.\n\
+\'Then it doesn\'t matter which way you go,\' said the Cat.\n\
+\'--so long as I get somewhere,\' Alice added as an explanation.\n\
+\'Oh, you\'re sure to do that,\' said the Cat, \'if you only walk long enough.\'")
