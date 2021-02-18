b390f9f90d8780aab4db5b8f93c088fe4621ba49
Will Swoveland <will.arctic@gmail.com>
2021-02-17 19:23:23 -0800

    Update tricky2.py

--- Week 02/Lab 02/tricky2.py
+++ Week 02/Lab 02/tricky2.py
@@ -1,14 +1,14 @@
 # Will Swoveland
 
 # I declare the variable, and then print it using string concatenation.
-# The str() function converts the integer 6 (x) into a string, which
-# allows me to insert it into another string.
+# We can either use str() to print the integer variables with the strings,
+# or we can pass them as extra parameters. Doing the latter is cleaner.
 
 x = 6
-print("x is " + str(x))
+print("x is", x)
 
 x = 30
-print("x is now " + str(x))
+print("x is now", x)
 
 y = x + 1
-print("x and y are " + str(x) + " and " + str(y))
+print("x and y are", x, "and", y)
